3ebe5974c11f8d7f50f25c32e5928586bfc4430f
1d1n <samsul.14@students.amikom.ac.id>
2020-03-13 03:50:57 +0700

    finishing

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,12 @@ class calculator
 {
 	static void Main (string[] args)
 	{
-
+		int a=10;
+		int b=6;
+		console.writeLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penjumlahan);
+		console.writeLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, pengurangan);
+		console.writeLine("\ntekan sembarang key untuk keluar..");
+		console.readKey();
 	}
 	static int penjumlahan(int a, int b)
 	{
